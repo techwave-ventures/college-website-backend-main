5a72130757407e96d44649ef4bf3b4f097a1fde1
techwave-ventures <techwaveventures@gmail.com>
2025-04-04 15:51:05 +0530

    Update examRoutes.js

--- routes/examRoutes.js
+++ routes/examRoutes.js
@@ -1,9 +1,9 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const {createExam} = require("../controller/examController")
+const { createExam, getExam } = require("../controller/examController");
 
-router.post("/",createExam);
+router.post("/", createExam);
+router.get("/", getExam); // Add this line to fetch exams
 
-
-module.exports = router
+module.exports = router;
