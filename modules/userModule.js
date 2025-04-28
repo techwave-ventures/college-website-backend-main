0f5489335368fcab39ec705eed4912126fab4649
akash-0963 <akashjare09@gmail.com>
2025-04-29 00:50:47 +0530

    fix: accountType default value

--- modules/userModule.js
+++ modules/userModule.js
@@ -4,12 +4,13 @@ const userSchema = new mongoose.Schema(
   {
       name: { type: String, required: true },
       email: { type: String, required: true, trim: true, unique: true }, // Added unique constraint
-      phoneNumber: { type: String },
+      phoneNumber: { type: String, required: true, trim: true, unique: true }, // Added unique constraint
       password: { type: String, required: true },
       accountType: {
           type: String,
           enum: ["Student", "Admin"], // Added "Admin"
-          required: true
+          required: true,
+          default: "Student",
       },
   },
   { timestamps: true }
@@ -20,7 +21,7 @@ const userSchema = new mongoose.Schema(
  *
  * - name: Full name of the user (required)
  * - email: Email address of the user (required, trimmed)
- * - phoneNumber: Contact number of the user (optional)
+ * - phoneNumber: Contact number of the user (required, trimmed)
  * - password: Hashed password of the user (required)
  * - accountType: Defines user role, currently supports "Student" only (required)
  * - timestamps: Automatically adds createdAt and updatedAt fields
