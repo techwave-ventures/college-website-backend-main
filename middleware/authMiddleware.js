86007226d4299c2d31f6513e782eca8aaa265e68
akash-0963 <akashjare09@gmail.com>
2025-04-29 02:15:11 +0530

    fix: authMiddleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -17,7 +17,7 @@ exports.auth = async (req, res, next) => {
 		}
 
 		try {
-			const decode = await jwt.verify(token, process.env.JWT_SECRET);
+			const decode = await jwt.verify(token, process.env.API_SECRET);
 			console.log(decode);
 			req.user = decode;
 		} catch (error) {
